5c715acc9391da66ac8282810742f39ea5af9785
Wathik Almayali <wathikalmayali@Wathik-Almayali.local>
2014-07-05 10:38:59 -0400

    Initial Commit

---
+++ TSC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TSC
+//
+//  Created by Wathik Almayali on 7/5/14.
+//  Copyright (c) 2014 Wathik Almayali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TSC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TSC
+//
+//  Created by Wathik Almayali on 7/5/14.
+//  Copyright (c) 2014 Wathik Almayali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
